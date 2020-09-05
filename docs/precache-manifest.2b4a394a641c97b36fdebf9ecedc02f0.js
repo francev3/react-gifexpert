c1039bdf6eaab817f26e36af2e095c37a40083c7
francisco Alvarado <franc.jadev@gmail.com>
2020-09-05 16:14:06 -0400

    added docs

---
+++ docs/precache-manifest.2b4a394a641c97b36fdebf9ecedc02f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42740ca37fb5dfa1766049622881741d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6816fd9dc86884b617e4",
+    "url": "/static/css/main.9da8c5b2.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6816fd9dc86884b617e4",
+    "url": "/static/js/main.55003d43.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
